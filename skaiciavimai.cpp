788309afbc5d83e0f178818514d505718f79c71c
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-12-09 13:14:06 +0200

    Create skaiciavimai.cpp

---
+++ skaiciavimai.cpp
@@ -0,0 +1,15 @@
+#include "lib.h"
+#include "skaiciavimai.h"
+#include "studentas.h"
+
+
+bool palygintivardus(Studentasc& a, Studentasc& b){
+  string a_vardas=a.getVardas(),a_pavarde=a.getPavarde(),b_vardas=b.getVardas(),b_pavarde=b.getPavarde();
+  return tie(a_vardas,a_pavarde)<tie(b_vardas,b_pavarde);
+}
+bool palygintigalutinius(Studentasc& a,Studentasc& b){
+  return a.getGal()>b.getGal();
+}
+bool palygintigalutiniusalt(Studentasc& a,Studentasc& b){
+  return a.getGal()<b.getGal();
+}
