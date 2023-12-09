a3d2cb018abc8b4648eae28f2d3960f4653ad12f
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-12-09 13:13:37 +0200

    Create skaiciavimai.h

---
+++ skaiciavimai.h
@@ -0,0 +1,26 @@
+#ifndef SKAICIAVIMAI_H
+#define SKAICIAVIMAI_H
+#include "lib.h"
+#include "studentas.h"
+
+class Timer {
+private:
+using hrClock = std::chrono::high_resolution_clock;
+using durationDouble = std::chrono::duration<double>;
+std::chrono::time_point<hrClock> start;
+public:
+Timer() : start{ hrClock::now() } {}
+void reset() {
+start = hrClock::now();
+}
+double elapsed() const {
+return durationDouble (hrClock::now() - start).count();
+}
+};
+
+
+
+bool palygintivardus(Studentasc& a, Studentasc& b);
+bool palygintigalutinius(Studentasc& a,Studentasc& b);
+bool palygintigalutiniusalt(Studentasc& a,Studentasc& b);
+#endif
