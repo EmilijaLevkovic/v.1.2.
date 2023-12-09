3b3387d3762f55646cad4bc62d9e8455f2368b6f
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-12-09 13:04:21 +0200

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,35 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include<vector>
+#include<fstream>
+#include<tuple>
+#include<random>
+#include <chrono>
+
+
+using std::cin;
+using std::copy;
+using std::cout;
+using std::endl;
+using std::left;
+using std::right;
+using std::setprecision;
+using std::setw;
+using std::string;
+using std::sort;
+using std::rand;
+using std::fixed;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::istringstream;
+using std::stoi;
+using std::tie;
+
+#endif
